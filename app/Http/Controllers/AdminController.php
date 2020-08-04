f816c60b8741fde2c4cb41d9f11904941404662d
HaikalPWPB <habiboded135@gmail.com>
2020-08-04 11:06:19 +0700

    Storage Link

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -39,6 +39,7 @@ class AdminController extends Controller
         $group->name = $request->get('name');
         $group->description = $request->get('description');
         $group->mode = $request->get('mode');
+        $group->save();
 
         return response()->json(compact('group'), 201);
     }
